5bdb608e2e9621c62abae9397426a26ad7f75b42
Aarenoksitolikus <super.naxa2001@yandex.ru>
2022-04-16 02:54:23 +0300

    add login controller

---
+++ src/main/java/ru/dev/bottled/cola/hackathon/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package ru.dev.bottled.cola.hackathon.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getSignInPage() {
+        return "sign_in_page";
+    }
+}
