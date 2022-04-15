871d983ab5fe5db32512322db5f0f00a9bcb36bc
Aarenoksitolikus <super.naxa2001@yandex.ru>
2022-04-16 02:55:53 +0300

    add controllers second version

--- src/main/java/ru/dev/bottled/cola/hackathon/controllers/SignUpController.java
+++ src/main/java/ru/dev/bottled/cola/hackathon/controllers/SignUpController.java
@@ -1,7 +1,33 @@
 package ru.dev.bottled.cola.hackathon.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.dev.bottled.cola.hackathon.dto.UserForm;
+import ru.dev.bottled.cola.hackathon.services.templates.UsersService;
 
 @Controller
 public class SignUpController {
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping("/signup")
+    public String getSignUpPage(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "sign_up_page";
+    }
+
+    @PostMapping("/signup")
+    public String signUp(UserForm form, Model model) {
+        usersService.signUp(form);
+        model.addAttribute("username", form.getUsername());
+        return "redirect:/confirm";
+    }
+
+    @GetMapping("/confirm")
+    public String getCOnfirmPage() {
+        return "confirm_page";
+    }
 }
